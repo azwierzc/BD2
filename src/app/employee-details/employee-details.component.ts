7f2f7683d84914245fc38bd8ebb606008da172b0
adabrowska11 <agnieszka.dabrowska.11@gmail.com>
2019-06-09 13:37:55 +0200

    mock add room reservation modal

--- src/app/employee-details/employee-details.component.ts
+++ src/app/employee-details/employee-details.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import {ActivatedRoute} from '@angular/router';
 import {EmployeeService} from '../services/employee.service';
 import {EmployeeModel} from '../employees/models/EmployeeModel';
+import {NgbModal} from '@ng-bootstrap/ng-bootstrap';
+import {RoomReservationComponent} from './room-reservation/room-reservation.component';
 
 @Component({
   selector: 'app-employee-details',
@@ -12,11 +14,14 @@ export class EmployeeDetailsComponent implements OnInit {
   employeeId: number;
   employee: EmployeeModel;
 
-  constructor(private route: ActivatedRoute, private service: EmployeeService) { }
+  constructor(private route: ActivatedRoute, private service: EmployeeService, private modalService: NgbModal) { }
 
   ngOnInit() {
     this.employeeId = Number(this.route.snapshot.params.id);
     this.service.fetchEmployee(this.employeeId).then((employee: EmployeeModel) => this.employee = employee);
   }
 
+  onAddRoomReservation() {
+    this.modalService.open(RoomReservationComponent, {ariaLabelledBy: 'modal-basic-title'});
+  }
 }
--- src/app/employee-details/employee-details.module.ts
+++ src/app/employee-details/employee-details.module.ts
@@ -1,13 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {EmployeeDetailsComponent} from './employee-details.component';
+import { RoomReservationComponent } from './room-reservation/room-reservation.component';
 
 @NgModule({
   declarations: [
-    EmployeeDetailsComponent
+    EmployeeDetailsComponent,
+    RoomReservationComponent
   ],
   imports: [
     CommonModule
+  ],
+  entryComponents: [
+    RoomReservationComponent
   ]
 })
 export class EmployeeDetailsModule { }
